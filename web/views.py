8ca073693a665c9b555bfb286a4abd88c4257816
ping.cai <asyty.cp@gmail.com>
2014-10-14 20:35:58 +0800

    改为码上说

--- web/views.py
+++ web/views.py
@@ -21,18 +21,20 @@ def wap_media(request, code):
     use_time = None
 
     qrcode = get_object_or_404(Qrcode, full=code)
-    qrcodeContent = CodeContent.objects.get(qrcode=qrcode)
-    text = qrcodeContent.text
-    use_time = qrcodeContent.last_update
-    medias = CodeMedia.objects.filter(relate_to=qrcodeContent)
-    for m in medias:
-        if m.media_type == 1:
-            audio = m
-        elif m.media_type == 2:
-            imgs.append(m)
-        elif m.media_type == 3:
-            video = m
-
+    try:
+        qrcodeContent = CodeContent.objects.get(qrcode=qrcode)
+        text = qrcodeContent.text
+        use_time = qrcodeContent.last_update
+        medias = CodeMedia.objects.filter(relate_to=qrcodeContent)
+        for m in medias:
+            if m.media_type == 1:
+                audio = m
+            elif m.media_type == 2:
+                imgs.append(m)
+            elif m.media_type == 3:
+                video = m
+    except:
+        pass
     return render_to_response('wap/media.html', {
         'text': text,
         'imgs': imgs,
